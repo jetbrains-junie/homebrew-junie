9c9e3118caba8dd9e68dbffda1e39ca9b6fe01f0
TeamCity <teamcity@matterhorn.com>
2025-10-22 17:18:18 +0000

    Update junie-cloud to 458.1

--- Formula/junie.rb
+++ Formula/junie.rb
@@ -1,9 +1,9 @@
 class Junie < Formula
   desc "Junie CLI"
   homepage "https://www.jetbrains.com/junie"
-  url "https://github.com/jetbrains-junie/junie/releases/download/99.1/junie-cloud-eap-251.99.1-macos-aarch64.zip"
-  sha256 ""
-  version "251.99.1"
+  url "https://github.com/jetbrains-junie/junie/releases/download/458.1/junie-cloud-eap-458.1-macos-aarch64.zip"
+  sha256 "a81755a44a12bab92148aa7f93f33af1a79aa44e8705e3455096042de362a11e"
+  version "458.1"
   license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
   def install
     libexec.install "junie.app"
