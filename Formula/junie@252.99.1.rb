82422a399c67a1f10e5ca1609f93ea7fdf4b1062
TeamCity <teamcity@matterhorn.com>
2025-10-22 16:23:04 +0000

    Update junie-cloud to 252.99.1

---
+++ Formula/junie@252.99.1.rb
@@ -0,0 +1,22 @@
+class JunieAT252991 < Formula
+  desc "Junie CLI"
+  homepage "https://www.jetbrains.com/junie"
+  url "https://github.com/jetbrains-junie/junie/releases/download/99.1/junie-cloud-eap-252.99.1-macos-aarch64.zip"
+  sha256 ""
+  version "252.99.1"
+  license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
+  def install
+    libexec.install "junie.app"
+    (bin/"junie").write <<~EOS
+      #!/bin/bash
+      EJ_RUNNER_PWD=$(pwd)
+      export EJ_RUNNER_PWD
+      cd "#{libexec}/junie.app/Contents/MacOS"
+      exec ./junie "$@"
+    EOS
+    chmod 0755, bin/"junie"
+  end
+  test do
+    system "#{bin}/junie", "--version"
+  end
+end
