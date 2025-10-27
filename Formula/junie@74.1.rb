5bcba93d1edca4ee6fb499eb65700f69c33aa373
TeamCity <teamcity@matterhorn.com>
2025-10-27 20:36:14 +0000

    Update junie to 474.1

---
+++ Formula/junie@74.1.rb
@@ -0,0 +1,22 @@
+class JunieAT4741 < Formula
+  desc "Junie CLI"
+  homepage "https://www.jetbrains.com/junie"
+  url "https://github.com/jetbrains-junie/junie/releases/download/474.1/junie-eap-474.1-macos-aarch64.zip"
+  sha256 "38e1fd965229005e0543d645d724aaf4e790ca20033ae034bb2f6115d4a18fa2"
+  version "474.1"
+  license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
+  def install
+    libexec.install "junie.app"
+    (bin/"junie").write <<~EOS
+      #!/bin/bash
+      EJ_RUNNER_PWD=$(pwd)
+      export EJ_RUNNER_PWD
+      cd "#{libexec}/junie.app/Contents/MacOS"
+      exec ./junie "$@"
+    EOS
+    chmod 0755, bin/"junie"
+  end
+  test do
+    system "#{bin}/junie", "--version"
+  end
+end
