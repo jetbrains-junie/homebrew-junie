f3eb8d89bfbcf6b8ca38c17d8af507a47d04a153
TeamCity <teamcity@matterhorn.com>
2025-11-10 17:18:45 +0000

    Update junie to 496.3

---
+++ Formula/junie@96.3.rb
@@ -0,0 +1,22 @@
+class JunieAT4963 < Formula
+  desc "Junie CLI"
+  homepage "https://www.jetbrains.com/junie"
+  url "https://github.com/jetbrains-junie/junie/releases/download/496.3/junie-eap-496.3-macos-aarch64.zip"
+  sha256 "8765095c9a0d3422e087fdbb161dcdfb69a5ef9fe85c1bfa21c087bd9a0b38d6"
+  version "496.3"
+  license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
+  def install
+    libexec.install "junie.app"
+    (bin/"junie").write <<~EOS
+      #!/bin/bash
+      EJ_RUNNER_PWD=$(pwd)
+      export EJ_RUNNER_PWD
+      cd "#{libexec}/junie.app/Contents/MacOS"
+      exec ./junie "$@"
+    EOS
+    chmod 0755, bin/"junie"
+  end
+  test do
+    system "#{bin}/junie", "--version"
+  end
+end
