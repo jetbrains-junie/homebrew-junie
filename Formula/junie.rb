8f5ed276e292d02754ae52f8214567ec73bc31dc
TeamCity <teamcity@matterhorn.com>
2025-11-17 13:11:12 +0000

    Update junie to 507.2

--- Formula/junie.rb
+++ Formula/junie.rb
@@ -1,9 +1,9 @@
 class Junie < Formula
   desc "Junie CLI"
   homepage "https://www.jetbrains.com/junie"
-  url "https://github.com/jetbrains-junie/junie/releases/download/496.3/junie-eap-496.3-macos-aarch64.zip"
-  sha256 "8765095c9a0d3422e087fdbb161dcdfb69a5ef9fe85c1bfa21c087bd9a0b38d6"
-  version "496.3"
+  url "https://github.com/jetbrains-junie/junie/releases/download/507.2/junie-eap-507.2-macos-aarch64.zip"
+  sha256 "8ceeb04b24d4e2074eed6817ed32471b809030271f1dc70a6e75a2a763018399"
+  version "507.2"
   license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
   def install
     libexec.install "junie.app"
