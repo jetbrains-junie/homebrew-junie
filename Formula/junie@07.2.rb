2b3b210aeb1b0bcd846d9267b03fd371f8f1a501
TeamCity <teamcity@matterhorn.com>
2025-11-17 13:11:09 +0000

    Update junie to 507.2

---
+++ Formula/junie@07.2.rb
@@ -0,0 +1,22 @@
+class JunieAT5072 < Formula
+  desc "Junie CLI"
+  homepage "https://www.jetbrains.com/junie"
+  url "https://github.com/jetbrains-junie/junie/releases/download/507.2/junie-eap-507.2-macos-aarch64.zip"
+  sha256 "8ceeb04b24d4e2074eed6817ed32471b809030271f1dc70a6e75a2a763018399"
+  version "507.2"
+  license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
+  def install
+    libexec.install "junie.app"
+    (bin/"junie").write <<~EOS
+      #!/bin/bash
+      EJ_RUNNER_PWD=$(pwd)
+      export EJ_RUNNER_PWD
+      cd "#{libexec}/junie.app/Contents/MacOS"
+      exec ./junie "$@"
+    EOS
+    chmod 0755, bin/"junie"
+  end
+  test do
+    system "#{bin}/junie", "--version"
+  end
+end
