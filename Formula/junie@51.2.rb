4acef6d473337168d72dd2e659e36bc90be92026
TeamCity <teamcity@matterhorn.com>
2025-12-08 12:55:33 +0000

    Update junie to 551.2

---
+++ Formula/junie@51.2.rb
@@ -0,0 +1,22 @@
+class JunieAT5512 < Formula
+  desc "Junie CLI"
+  homepage "https://www.jetbrains.com/junie"
+  url "https://github.com/jetbrains-junie/junie/releases/download/551.2/junie-eap-551.2-macos-aarch64.zip"
+  sha256 "bfaaa182a67709868cb75287360cbbdbb655b64ead325eadc99c3ef69c03214a"
+  version "551.2"
+  license "https://www.jetbrains.com/legal/docs/terms/jetbrains-junie"
+  def install
+    libexec.install "junie.app"
+    (bin/"junie").write <<~EOS
+      #!/bin/bash
+      EJ_RUNNER_PWD=$(pwd)
+      export EJ_RUNNER_PWD
+      cd "#{libexec}/junie.app/Contents/MacOS"
+      exec ./junie "$@"
+    EOS
+    chmod 0755, bin/"junie"
+  end
+  test do
+    system "#{bin}/junie", "--version"
+  end
+end
